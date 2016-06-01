7459ad91579c4b43aa51fa6f57b4a52543898a62
vierello <vierello@hotmail.com>
2016-06-01 14:34:17 -0400

    question four

--- refactor.js
+++ refactor.js
@@ -110,6 +110,9 @@ var reverseStr = function(str) {
 
 // Put your answer below -------------------------
 
+var reverseStr = function(str) {
+  return str.split("").reverse().join("");
+};
 
 // -----------------------------------------------
 
